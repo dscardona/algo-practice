e276eb079a7e40380efda9316a1cc766e7de5752
Diana Cardona <dscardona@gmail.com>
2023-05-31 16:42:34 -0400

    Add validate subsequence

---
+++ python/2-validateSubsequence.py
@@ -0,0 +1,19 @@
+def isValidSubsequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+def isValidSubsequence(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx == len(sequence):
+            break
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
+
