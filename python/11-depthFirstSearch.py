669a1358614bfa0075480295ddb1f4862b41faed
Diana Cardona <dscardona@gmail.com>
2023-06-12 17:07:18 -0400

    Add depthFirstSearch algorithm for graph traversal

---
+++ python/11-depthFirstSearch.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
+# Create nodes
+nodeA = Node("A")
+nodeB = Node("B")
+nodeC = Node("C")
+nodeD = Node("D")
+nodeE = Node("E")
+nodeF = Node("F")
+nodeG = Node("G")
+nodeH = Node("H")
+nodeI = Node("I")
+
+# Connect nodes
+nodeA.addChild(nodeB)
+nodeA.addChild(nodeC)
+nodeA.addChild(nodeD)
+nodeB.addChild(nodeE)
+nodeB.addChild(nodeF)
+nodeD.addChild(nodeG)
+nodeD.addChild(nodeH)
+nodeF.addChild(nodeI)
+
+# Test the depthFirstSearch function
+result = nodeA.depthFirstSearch([])
+print(result)
